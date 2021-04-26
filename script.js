2439164c69faaa662a328d7bde5fa32b7a89a504
Stef Geraets <stef@getnoticed.nl>
2021-04-26 11:35:54 +0200

    Small cleanup

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const game = {
-  container: document.querySelector(".grid"),
-  map: document.querySelector(".map"),
-  wall: document.querySelector(".animated-wall"),
-  timer: document.querySelector(".timer"),
-  timeBar: document.querySelector(".timebar"),
-  countdownWrapper: document.querySelector('.countdownWrapper'),
-  countdownTime: document.querySelector('.countdownTime'),
-  settings: document.querySelector('.settings'),
-  gridNodes: "",
-  loop: "",
-  root: document.documentElement,
+  el: {
+    container: document.querySelector(".grid"),
+    wall: document.querySelector(".animated-wall"),
+    timer: document.querySelector(".timer"),
+    timeBar: document.querySelector(".timebar"),
+    countdownWrapper: document.querySelector('.countdownWrapper'),
+    countdownTime: document.querySelector('.countdownTime'),
+    settings: document.querySelector('.settings'),
+    gridNodes: "",
+    root: document.documentElement,
+  },
   keys: {
     space: 32,
     left: 37,
@@ -17,10 +17,9 @@ const game = {
     right: 39,
     down: 40,
   },
-  clickCount: 0,
-  avatar: {
+  token: {
     element: document.createElement("div"),
-    squareSize: 24,
+    size: 24,
     color: "#ff0000",
     position: {
       x: 0,
@@ -37,6 +36,8 @@ const game = {
     ring: "trumpet.mp3",
     volume: 0.1,
   },
+  clickCount: 0,
+  loop: "",
   safeZoneWidth: [20, 10, 5, 2],
   centerSelector: ".grid-item-2-2",
   gameState: "paused", // started || paused || ended
@@ -97,9 +98,9 @@ function addTimesInMilliseconds() {
 }
 
 function makeGrid(rows, cols) {
-  game.container.style.setProperty("--grid-rows", rows);
-  game.container.style.setProperty("--grid-cols", cols);
-  game.root.style.setProperty("--cell-width", game.grid.cellSize + "px");
+  game.el.container.style.setProperty("--grid-rows", rows);
+  game.el.container.style.setProperty("--grid-cols", cols);
+  game.el.root.style.setProperty("--cell-width", game.grid.cellSize + "px");
 
   let x = 0;
   let y = 0;
@@ -112,7 +113,7 @@ function makeGrid(rows, cols) {
       x++
     }
 
-    game.container.appendChild(cell).className = "grid-item grid-item-" + x + "-" + y;
+    game.el.container.appendChild(cell).className = "grid-item grid-item-" + x + "-" + y;
   }
 }
 
@@ -121,20 +122,20 @@ function initGame() {
 
   makeGrid(game.grid.rows, game.grid.cols);
 
-  let avatar = game.avatar.element;
-  avatar.style.width = game.avatar.squareSize + "px";
-  avatar.style.height = game.avatar.squareSize + "px";
-  avatar.style.backgroundColor = game.avatar.color;
+  let token = game.token.element;
+  token.style.width = game.token.size + "px";
+  token.style.height = game.token.size + "px";
+  token.style.backgroundColor = game.token.color;
 
-  game.gridNodes = document.querySelectorAll(".grid-item");
+  game.el.gridNodes = document.querySelectorAll(".grid-item");
   let firstGridItem = document.querySelector(".grid-item");
-  firstGridItem.appendChild(avatar);
+  firstGridItem.appendChild(token);
 
   game.centerSelector = `.grid-item-${game.grid.rows / 2}-${game.grid.cols / 2}`;
-  game.root.style.setProperty('--closing-time', `${game.time.circleClosing}ms`);
-  game.root.style.setProperty('--circle-delay', `${game.time.circleDelay}ms`);
-  game.root.style.setProperty('--game-time', `${game.time.total}ms`);
-  game.root.style.setProperty('--countdown-height', `${game.time.countdown}vh`);
+  game.el.root.style.setProperty('--closing-time', `${game.time.circleClosing}ms`);
+  game.el.root.style.setProperty('--circle-delay', `${game.time.circleDelay}ms`);
+  game.el.root.style.setProperty('--game-time', `${game.time.total}ms`);
+  game.el.root.style.setProperty('--countdown-height', `${game.time.countdown}vh`);
 
   timelineGen();
 
@@ -161,12 +162,12 @@ function pauseGame(e) {
     }
     if (game.gameState === "paused") {
       game.gameState = "started"
-      game.wall.style.animationPlayState = 'running'
-      game.timer.style.animationPlayState = 'running'
+      game.el.wall.style.animationPlayState = 'running'
+      game.el.timer.style.animationPlayState = 'running'
     } else {
       game.gameState = "paused"
-      game.wall.style.animationPlayState = 'paused'
-      game.timer.style.animationPlayState = 'paused'
+      game.el.wall.style.animationPlayState = 'paused'
+      game.el.timer.style.animationPlayState = 'paused'
     }
   }  
 }
@@ -195,7 +196,7 @@ function timelineGen() {
     let time = document.createElement('div');
     time.classList.add('timeblock');
     time.style.height = `${timeBlock}vh`
-    game.timeBar.appendChild(time);
+    game.el.timeBar.appendChild(time);
   })
 }
 
@@ -207,14 +208,14 @@ function countdownClock() {
   
   formatClock(timeObj.minutes, timeObj.seconds);
 
-  game.countdownWrapper.classList.add("show");
+  game.el.countdownWrapper.classList.add("show");
   timer.onTick(formatClock).onTick(removeClock).start();
 }
 
 function removeClock() {
   if (this.expired()) {
     setTimeout(() => {
-      game.countdownWrapper.classList.remove('show')
+      game.el.countdownWrapper.classList.remove('show')
     }, 1000);
   }
 }
@@ -222,7 +223,7 @@ function removeClock() {
 function formatClock(minutes, seconds) {
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  game.countdownTime.textContent = `${minutes}:${seconds}`;
+  game.el.countdownTime.textContent = `${minutes}:${seconds}`;
 }
 
 function handleKey(e) {
@@ -231,22 +232,22 @@ function handleKey(e) {
   }
   switch (e.keyCode || e) {
     case game.keys.left:
-      game.avatar.position.y--;
+      game.token.position.y--;
       break;
     case game.keys.up:
-      game.avatar.position.x--;
+      game.token.position.x--;
       break;
     case game.keys.right:
-      game.avatar.position.y++;
+      game.token.position.y++;
       break;
     case game.keys.down:
-      game.avatar.position.x++;
+      game.token.position.x++;
       break;
     default: return
   }
 
-  let gridItem = document.querySelector(".grid-item-" + game.avatar.position.x + "-" + game.avatar.position.y);
-  gridItem.appendChild(game.avatar.element);
+  let gridItem = document.querySelector(".grid-item-" + game.token.position.x + "-" + game.token.position.y);
+  gridItem.appendChild(game.token.element);
 }
 
 function storeTarget(item) {
@@ -263,21 +264,21 @@ function handleCirclePlacement(item) {
   if (game.clickCount === 0) {
     placeWall(item);
   } else if (game.clickCount < game.safeZoneWidth.length) {
-    game.wall.classList.remove('closeCircle')
+    game.el.wall.classList.remove('closeCircle')
 
-    let startTop = game.root.style.getPropertyValue("--end-top");
-    let startLeft = game.root.style.getPropertyValue("--end-left");
+    let startTop = game.el.root.style.getPropertyValue("--end-top");
+    let startLeft = game.el.root.style.getPropertyValue("--end-left");
     let pos = getItemOffset(item);
   
     setWallStartPosition(startTop, startLeft);
     setWallEndPosition(pos.top, pos.left);
     
-    game.root.style.setProperty('--start-width', game.safeZoneWidth[game.clickCount - 1] * game.grid.cellSize + "px");
-    game.root.style.setProperty('--circle-width', game.safeZoneWidth[game.clickCount]);
+    game.el.root.style.setProperty('--start-width', game.safeZoneWidth[game.clickCount - 1] * game.grid.cellSize + "px");
+    game.el.root.style.setProperty('--circle-width', game.safeZoneWidth[game.clickCount]);
 
-    game.wall.classList.add('closeCircle')
+    game.el.wall.classList.add('closeCircle')
   }
-  // game.wall.addEventListener("animationstart", function(){
+  // game.el.wall.addEventListener("animationstart", function(){
   //   playSound(game.sound.closing)
   // }, false);
   placeCircle(item);
@@ -285,7 +286,7 @@ function handleCirclePlacement(item) {
 }
 
 function placeCircle(gridCell) {
-  game.gridNodes.forEach(function(gridItem) {
+  game.el.gridNodes.forEach(function(gridItem) {
     gridItem.classList.remove("selected")
   })
 
@@ -304,19 +305,19 @@ function placeWall(item) {
   setWallStartPosition(offset.top, offset.left);
   setWallEndPosition(offset.top, offset.left);
   
-  game.wall.classList.add('closeCircle')
+  game.el.wall.classList.add('closeCircle')
 }
 
 function setWallStartPosition(top, left) {
   top = parseInt(top, 10);
   left = parseInt(left, 10);
-  game.root.style.setProperty("--start-top", top + "px");
-  game.root.style.setProperty("--start-left", left + "px");
+  game.el.root.style.setProperty("--start-top", top + "px");
+  game.el.root.style.setProperty("--start-left", left + "px");
 }
 
 function setWallEndPosition(top, left) {
-  game.root.style.setProperty("--end-top", top + "px");
-  game.root.style.setProperty("--end-left", left + "px");
+  game.el.root.style.setProperty("--end-top", top + "px");
+  game.el.root.style.setProperty("--end-left", left + "px");
 }
 
 function getItemOffset(item) {
@@ -338,31 +339,28 @@ const channel = new BroadcastChannel("wubg");
 
 if(window.name === game.settingWindow) {
   game.grid.cellSize = 24;
-  game.avatar.squareSize = 20;
-  game.root.style.setProperty("--show-timebar", "block");
+  game.token.size = 20;
+  game.el.root.style.setProperty("--show-timebar", "block");
 }
-
-
-
 
 initGame();
 
 if(window.name === game.settingWindow) {
-  game.gridNodes.forEach(function(item) {
+  game.el.gridNodes.forEach(function(item) {
     item.addEventListener("click", function(item) {
       channel.postMessage({action: "place", target: item.target.classList[1]})
     })
   });
 }
 
-if(game.settings !== null) {
-  game.settings.addEventListener("click", openSettings);
+if(game.el.settings !== null) {
+  game.el.settings.addEventListener("click", openSettings);
 }
+
 window.addEventListener("keydown", pauseGame);
-game.gridNodes.forEach(function(item) {
+game.el.gridNodes.forEach(function(item) {
   item.addEventListener("click", storeTarget);
 })
-
 
 channel.onmessage = function(e) {
   if (e.data.action === "pause") {
@@ -378,11 +376,7 @@ channel.onmessage = function(e) {
 }
 
 
-
-
-
-
-
+// Countdown Clock: Credits robbmj https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript
 function CountDownTimer(duration, granularity) {
   this.duration = duration;
   this.granularity = granularity || 1000;
